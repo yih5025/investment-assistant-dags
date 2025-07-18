47319d991b7f177722300c1d45399079e76c4a46
yih5025 <yih5025@naver.com>
2025-07-18 15:49:12 +0900

    modified upser calendar sql

--- dags/sql/upsert_earnings_calendar.sql
+++ dags/sql/upsert_earnings_calendar.sql
@@ -1,26 +1,23 @@
 INSERT INTO earnings_calendar (
     symbol, 
-    name, 
+    company_name, 
     report_date, 
     fiscal_date_ending, 
     estimate, 
     currency,
-    created_at,
-    updated_at
+    created_at
 ) VALUES (
     %(symbol)s, 
-    %(name)s, 
+    %(company_name)s, 
     %(report_date)s, 
     %(fiscal_date_ending)s, 
     %(estimate)s, 
     %(currency)s,
-    NOW(),
     NOW()
 )
 ON CONFLICT (symbol, report_date)
 DO UPDATE SET
-    name = EXCLUDED.name,
+    company_name = EXCLUDED.company_name,
     fiscal_date_ending = EXCLUDED.fiscal_date_ending,
     estimate = EXCLUDED.estimate,
-    currency = EXCLUDED.currency,
-    updated_at = NOW();
+    currency = EXCLUDED.currency;
