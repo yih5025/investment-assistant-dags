e13b16bff088f4a3a5f65d2eeed76b61366ddfce
yih5025 <yih5025@naver.com>
2025-09-24 17:53:55 +0900

    added social media analysis code

--- batch_processing_social_media_analysis_dag.py
+++ batch_processing_social_media_analysis_dag.py
@@ -52,7 +52,8 @@ def social_media_analysis_dag():
         WHERE tweet_id NOT IN (
             SELECT post_id FROM post_analysis_cache WHERE post_source = 'x'
         )
-        AND text NOT LIKE '@%%' -- '%'를 '%%'로 수정
+        AND text NOT LIKE '@%%'
+        AND text IS NOT NULL
         ORDER BY created_at DESC
         LIMIT %s
         """
